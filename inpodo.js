ffc1806f03eb64ce55aeda15a47dcf703e88057d
Jesuferanmi Agboola <117743728+JFstudios03@users.noreply.github.com>
2023-11-01 11:42:26 +0100

    Update inpodo.js

--- inpodo.js
+++ inpodo.js
@@ -58,3 +58,18 @@ function showList() {
     list.innerHTML = localStorage.getItem("data");
 }
 showList();
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    else if (i % 5 === 0) {
+        console.log("BUZZ");
+    } else if (i % 3 === 0) {
+        console.log("FIZZ");
+    }
+    else{
+        console.log(i);
+    }
+    
+}
